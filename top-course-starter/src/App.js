0a728098858d1b03d317a302b9ad4d35244ac626
namanvats12 <namanvats12@gmail.com>
2023-04-03 16:19:38 +0530

    courses

---
+++ top-course-starter/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Filters from "./components/Filters";
+import Cards from "./components/Cards";
+import { filterData, apiUrl } from "./data";
+import { toast } from "react-toastify";
+import Spinner from "./components/Spinner";
+
+const App = () => {
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory]=useState(filterData[0].title)
+
+  async function fetchData() {
+    setLoading(true);
+    try {
+      let respose = await fetch(apiUrl);
+      let output = await respose.json();
+      // output-->
+      setCourses(output.data);
+    } catch (error) {
+      toast.error("somethig went wrong");
+      console.log(error)
+    }
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="min-h-screen flex flex-col bg-bgDark2">
+      <div>
+        <Navbar />
+      </div>
+      <div className="bg-bgDark2">
+        <Filters filterData={filterData} category={category} setCategory={setCategory}/>
+      </div>
+      <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+        {loading ? <Spinner /> : <Cards courses={courses} category={category} />}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ top-course-starter/src/components/Cards.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Card from "./Card";
+
+const Cards = (props) => {
+  let courses = props.courses;
+  let category = props.category;
+  console.log("All courses", courses);
+  const [likedCourses, setLikedCourses] = useState([]);
+  // map() applied on array always. so, 1st we use forEach() for every array of object
+  // then again apply forEach() for particular array of object. && object.value() focus only all the value avoid keys.
+
+  function getCourses() {
+    let allCourses = [];
+
+    if (category === "All") {
+      Object.values(courses).forEach((array) => {
+        array.forEach((couseData) => {
+          allCourses.push(couseData);
+        });
+      });
+      return allCourses;
+    } else {
+      // I will print only specified category data
+      return courses[category];
+    }
+  }
+
+  return (
+    <div className="flex flex-wrap justify-center gap-4 mb-4">
+      {getCourses().map((sigleCourse) => {
+        return (
+          <Card
+            key={sigleCourse.id}
+            sigleCourse={sigleCourse}
+            likedCourses={likedCourses}
+            setLikedCourses={setLikedCourses}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cards;
